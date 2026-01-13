92d518e9e13dd3a250fcc80e9a6a15835b028f0d
alikerock <alikerock@gmail.com>
2026-01-13 14:24:56 +0900

    feat: profile 라우터 추가

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,19 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Home from '../routes/Home';
 import Auth from "../routes/Auth";
+import Nav from "./Nav";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn})=>{ 
 
   return(
-    <Routes>
-      {
-        isLoggedIn ? 
-        <Route path="/" element={<Home/>}></Route> 
-        : 
-        <Route path="/" element={<Auth/>}></Route> 
-      }
-      
-    </Routes>
+    <>
+      {isLoggedIn && <Nav/>}
+      <Routes>
+        {
+          isLoggedIn ? 
+          <>
+            <Route path="/" element={<Home/>}></Route> 
+            <Route path="/profile" element={<Profile/>}></Route> 
+          </>
+          : 
+          <Route path="/" element={<Auth/>}></Route> 
+        }
+        
+      </Routes>
+    </>
   )
 }
 export default AppRouter;
