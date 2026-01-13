4ef1b10c5d33a5e479548486a6061b5fae761d72
alikerock <alikerock@gmail.com>
2026-01-13 14:25:09 +0900

    feat: 로그아웃 추가

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,6 +1,25 @@
-const Profile = ()=>{
-  return(
-    <span>Profile</span>
+import Button from 'react-bootstrap/Button';
+import { signOut } from "firebase/auth";
+import { authService } from '../firebase.js';
+import { useNavigate } from 'react-router-dom';
+
+const Profile = () => {
+  const auth = authService;
+  let navigate = useNavigate(); 
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      alert('로그아웃되었습니다.');
+      navigate("/");
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  return (
+    <>
+      <h2>Profile Page</h2>
+      <Button variant="primary" onClick={handleLogout}>로그아웃</Button>
+    </>
   )
 }
 export default Profile;
