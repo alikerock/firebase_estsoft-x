cf93883466419e93070f915e95c9904ea872d6c5
alikerock <alikerock@gmail.com>
2026-01-13 10:51:19 +0900

    feat: login, register form

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,6 +1,54 @@
-const Auth = ()=>{
-  return(
-    <span>Auth</span>
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+const Auth = () => {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const [input,setInput] = useState({email:'', password:''});
+  const [newAccount, setNewAccount] = useState(true); //회원가입,로그인 구분할 변수, 기본값은 회원가입 true
+
+  const handleChange = (e)=>{
+    /*
+    console.log(e.target.name);
+    console.log(e.target.value);
+    let name = e.target.name;
+    let value = e.target.value
+    const {name, value} = e.target;
+
+    if(name === 'email'){
+      setInput(value);
+    }else{
+      setInput(value)
+    }
+    */
+    const {name, value} = e.target;
+    setInput(prev=>({...prev, [name]:value }));
+
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(newAccount){
+      //회원가입 진행
+    }else{
+      //로그인 진행
+    }
+  }
+  return (
+    <>
+      <h2>{newAccount ? '회원가입': '로그인'}</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="userEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleChange}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="userPW">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password"    placeholder="Password" onChange={handleChange}/>
+        </Form.Group>
+         <Button variant="primary">{newAccount ? '회원가입': '로그인'}</Button>
+      </Form>
+    </>
   )
 }
 export default Auth;
