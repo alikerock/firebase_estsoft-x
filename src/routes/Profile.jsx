5ec5704099192abe8f49482ab510b93b3a2c631f
alikerock <alikerock@gmail.com>
2026-01-14 16:01:47 +0900

    feat:profile page 목록 출력

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,16 +1,20 @@
 import Button from 'react-bootstrap/Button';
-import { authService, storageService } from '../firebase.js';
+import { db, authService, storageService } from '../firebase.js';
 import { updateProfile, signOut } from "firebase/auth";
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import avata from "../default_icon.png";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useEffect, useState } from 'react';
-console.log(authService);
+import ListGroup from 'react-bootstrap/ListGroup';
+import { collection, where, onSnapshot, query, orderBy } from "firebase/firestore";
+import Comment from '../components/Comment';
 
 const Profile = () => {
   const auth = authService;
+  const user = auth.currentUser;
   let navigate = useNavigate(); 
   const [profile, setProfile] = useState(avata);
+  const [comments, setComments] = useState([]); //모든글 조회
 
   const handleLogout = () => {
     signOut(auth).then(() => {
@@ -24,18 +28,29 @@ const Profile = () => {
     const file = e.target.files?.[0];
     if(!file) return;
 
-    const storageRef = ref(storageService, `profile/${auth.currentUser.uid}`);
+    const storageRef = ref(storageService, `profile/${user.uid}`);
     const snapshot = await uploadBytes(storageRef, file);
     const imageURL = await getDownloadURL(snapshot.ref);//업로드된 이미지의 절대 경로 할당
     setProfile(imageURL);
 
-    updateProfile(auth.currentUser, {
+    updateProfile(user, {
       photoURL: imageURL
     });
   }
 
+  const getComments = async () => {
+    const q = query(collection(db, "comments"),orderBy('date', 'desc'), where("uid", "==", user.uid));
+    onSnapshot(q, (querySnapshot) => {
+      const commentsArray = querySnapshot.docs.map(doc=> ({
+        id:doc.id,
+        ...doc.data()
+      }));    
+      setComments(commentsArray);
+    });
+  }  
   useEffect(()=>{
-    auth.currentUser.photoURL.includes('firebase') && setProfile(auth.currentUser.photoURL);
+    user.photoURL.includes('firebase') && setProfile(user.photoURL);
+    getComments();
   },[]);
 
   return (
@@ -44,12 +59,19 @@ const Profile = () => {
       <div className="profile">
         <p className='d-flex gap-1 align-items-center'>
           <img src={profile} alt="프로필 이미지" />
-          {auth.currentUser.displayName}
+          {user.displayName}
         </p>
         <input type="file" accept='image/*' id="avata" className='hidden' onChange={handleChange} />
         <label htmlFor="avata" className='btn btn-secondary btn-sm'>이미지 업데이트</label>
       </div>
       <Button variant="primary" onClick={handleLogout}>로그아웃</Button>
+      <hr />
+      <ListGroup>
+        {comments.map(item => (
+          <Comment key={item.id} commentObj={item} isOwner={item.uid === user.uid} />
+        )
+        )}
+      </ListGroup>      
     </>
   )
 }
