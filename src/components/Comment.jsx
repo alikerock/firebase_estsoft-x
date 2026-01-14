0131b2a1eca8b9cb68464938b86e12ea65ed308f
alikerock <alikerock@gmail.com>
2026-01-14 09:18:27 +0900

    feat: 본인글 삭제 기능

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,17 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import { db } from '../firebase';
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Comment = ({ commentObj, isOwner }) => {
+  
+  const handleDelete = async ()=>{
+    const deleteConfirm = window.confirm('정말 삭제할까요?');
+    if(deleteConfirm){
+      await deleteDoc(doc(db, "comments", commentObj.id));
+    }
+  }
+
   return (
     <ListGroup.Item>
       <div className='d-flex justify-content-between'>
@@ -9,7 +19,7 @@ const Comment = ({ commentObj, isOwner }) => {
         {
           isOwner && <div className='d-flex gap-1'>
             <Button variant="secondary" size="sm">수정</Button>
-            <Button variant="danger" size="sm">삭제</Button>
+            <Button variant="danger" size="sm" onClick={handleDelete}>삭제</Button>
           </div>
         }        
       </div>
