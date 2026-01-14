3e14744b3aa2ca188ceda70944ff950b8b486ade
alikerock <alikerock@gmail.com>
2026-01-14 10:49:23 +0900

    feat:첨부파일 미리보기 및 취소

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,7 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { db } from '../firebase';
 import { collection, addDoc, serverTimestamp, getDocs, onSnapshot, query, orderBy } from "firebase/firestore";
 import Comment from '../components/Comment';
@@ -9,6 +9,10 @@ import Comment from '../components/Comment';
 const Home = ({ userId }) => {
   const [comment, setComment] = useState(''); //새글 입력
   const [comments, setComments] = useState([]); //모든글 조회
+  const [attachment, setAttachment] = useState(null);
+  const fileInputRef = useRef(null);
+
+
   const handleChange = (e) => {
     setComment(e.target.value);
   }
@@ -48,7 +52,23 @@ const Home = ({ userId }) => {
     getComments();
   }, []);//최초 한번 실행
 
-  console.log(comments);
+  const handleFileChange = (e)=>{
+    const {target:{files}} = e; // const files = e.target.files
+    const theFile = files[0];
+    console.log(theFile);
+    const reader = new FileReader();
+    reader.onloadend = (e)=>{ //첨부파일 읽기가 끝나면      
+      setAttachment(e.target.result);
+    }
+    reader.readAsDataURL(theFile); //첨부파일의 정보를 읽어
+    
+  }
+  const handleFileCancel = ()=>{
+    setAttachment(null); //파일정보가 담긴 변수 초기화
+    if(fileInputRef.current.value){
+      fileInputRef.current.value = ''; //파일정보 초기화
+    }
+  }
   return (
     <>
       <Form className="mt-3" onSubmit={handleSubmit}>
@@ -56,6 +76,16 @@ const Home = ({ userId }) => {
           <Form.Label>새 글을 입력해주세요</Form.Label>
           <Form.Control as="textarea" rows={3} value={comment} onChange={handleChange} />
         </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">          
+          <Form.Control type="file" accept="image/*" ref={fileInputRef} onChange={handleFileChange} />
+        </Form.Group>
+        {
+          attachment && 
+          <div className='mb-1'>
+            <img src={attachment} width='50px' height='50px' alt=''/>
+            <Button type="button" variant="secondary" size="sm" onClick={handleFileCancel}>취소</Button>
+          </div>
+        }
         <Button type="submit" variant="primary">새글 입력</Button>
       </Form>
       <hr />
